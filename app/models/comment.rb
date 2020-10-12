945372e76d24aefe297ede312c2d53b749f6f101
see-bet-code <brittani.elan.taylor@gmail.com>
2020-10-12 09:24:50 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  accepts_nested_attributes_for :user
-
-  def user_attributes=(user_attributes)
-    self.user = User.find_or_create_by(username: user_attributes[:username]) unless user_attributes[:username].blank? 
-  end
+  accepts_nested_attributes_for :user, 
+    reject_if: proc { |attributes| attributes[:username].blank?}
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,14 +6,14 @@ class Post < ActiveRecord::Base
 
   accepts_nested_attributes_for :categories, reject_if: proc { |category_attributes| category_attributes[:name].blank? }
 
-  def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-      if !category_attribute[:name].blank? 
-        category = Category.find_or_create_by(category_attribute)
-        self.categories << category
-      end
-    end
-  end
+  # def categories_attributes=(category_attributes)
+  #   category_attributes.values.each do |category_attribute|
+  #     if !category_attribute[:name].blank? 
+  #       category = Category.find_or_create_by(category_attribute)
+  #       self.categories << category
+  #     end
+  #   end
+  # end
 
 
 end
